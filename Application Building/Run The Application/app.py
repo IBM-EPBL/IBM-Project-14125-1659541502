28e2453349a1d315e78a9f3af0efe399c85062b8
Priyabindhu <112383803+Priyabindhu@users.noreply.github.com>
2022-11-23 07:47:22 +0530

    Add files via upload

--- Application Building/Run The Application/app.py
+++ Application Building/Run The Application/app.py
@@ -1,67 +1,82 @@
-from flask import Flask,render_template,request
-# Flask-It is our framework which we are going to use to run/serve our application.
-#request-for accessing file which was uploaded by the user on our application.
+from flask import Flask, render_template, request,session
 import os
-import numpy as np #used for numerical analysis
-from tensorflow.keras.models import load_model #to load our trained model
-from tensorflow.keras.preprocessing import image
-import requests
+from werkzeug.utils import secure_filename
+import numpy as np
+from keras.models import load_model
+from keras.utils import load_img,img_to_array
+import sqlite3
 
 
-app = Flask(__name__,template_folder="templates") #initializing a flask app 
-# Loading the model
-model=load_model('nutrition.h5')
-print("Loaded model from disk")
+UPLOAD_FOLDER=os.path.join('static','uploads')
+ALLOWED_EXTENSIONS = {'jpg','png','jpeg'}
 
-@ app.route('/')# route to display the home page
+app = Flask(__name__, template_folder="templates")
+app.config['UPLOAD_FOLDER']=UPLOAD_FOLDER
+app.secret_key = "nutrition"
+
+@app.route('/')
 def home():
-    return render_template('home.html') #rendering the home page
+    return render_template('home.html')
 
-@ app.route('/image1', methods=['GET', 'POST']) # routes to the index html
-def image1():
+@app.route('/aboutus')
+def aboutus():
+    return render_template("aboutus.html")
+
+@app.route('/image')
+def image():
     return render_template("image.html")
 
+@app.route('/imageprediction', methods=['GET', 'POST'])
+def imageprediction():
+    if request.method=="POST":
+        img  = request.files["image"]
+        img_filename = secure_filename(img.filename)
+        img.save(os.path.join(app.config['UPLOAD_FOLDER'],img_filename))
+        session['uploaded_img_filepath'] = os.path.join(app.config['UPLOAD_FOLDER'],img_filename)
+        img_filepath = session.get('uploaded_img_filepath',None)
+        image_pred = launch(img_filepath)
+        print(image_pred)
+        print("image_pred",image_pred[1])
+        fruit = 'FRUIT: ' + image_pred[1]['FRUIT']
+        serving_size = 'SERVING_SIZE: ' + image_pred[1]['SERVING_SIZE']
+        energy = 'ENERGY: ' + image_pred[1]['ENERGY']
+        fat = 'FAT: ' + image_pred[1]['FAT']
+        saturated_fat = 'SATURATED FAT: ' + image_pred[1]['SATURATED FAT']
+        mono_unsaturated_fat = 'MONO_UNSATURATED_FAT: ' + image_pred[1]['MONO_UNSATURATED_FAT']
+        poly_unsaturated_fat = 'POLY_UNSATURATED_FAT: ' + image_pred[1]['POLY_UNSATURATED_FAT']
+        carbohydrates = 'CARBOHYDRATES: ' + image_pred[1]['CARBOHYDRATES']
+        sugar = 'SUGAR: ' + image_pred[1]['SUGAR']
+        fiber = 'FIBER: ' + image_pred[1]['FIBER']
+        protein = 'PROTEIN: ' + image_pred[1]['PROTEIN']
+        sodium = 'SODIUM: ' + image_pred[1]['SODIUM']
+        cholesterol = 'CHOLESTEROL: ' + image_pred[1]['CHOLESTEROL']
+        potassium = 'POTASSIUM: ' + image_pred[1]['POTASSIUM']
+        output = 'OUTPUT: ' + image_pred[1]['OUTPUT']
+        return render_template("imageprediction.html", value=img_filepath, pred=image_pred[0],
+                fruit=fruit, serving_size=serving_size, energy=energy, fat=fat, saturated_fat=saturated_fat,
+                mono_unsaturated_fat=mono_unsaturated_fat, poly_unsaturated_fat=poly_unsaturated_fat,
+                carbohydrates=carbohydrates, sugar=sugar, fiber=fiber, protein=protein, sodium=sodium,
+                cholesterol=cholesterol, potassium=potassium, output=output, flag=True)
 
-@ app.route('/predict' ,methods=['GET','POST']) # route to show the predictions in a Web UI
-def lanuch():
-    if request.method=='POST':
-        f=request.files['file'] # requesting the file
-        basepath=os.path.dirname('__file__') #storing the file directory
-        filepath=os.path.join(basepath,"uploads",f.filename) #storing the file in uploads folder
-        f.save(filepath) #saving the file
-        
-        img=image.load_img(filepath,target_size=(64,64)) #load and reshaping the image
-        x=image.img_to_array(img) #converting image to an array
-        x=np.expand_dims(x,axis=0) #changing the dimensions of the image
-        
-        pred=np.argmax(model.predict(x), axis=1)
-        print("prediction",pred) #printing the prediction
-        index=['APPLE','BANANA','ORANGE','PINEAPPLE','WATERMELON']
-        
-        result=str(index[pred[0]])
-        print(result)
-        x=result
-        result=nutrition(result)
-        print(result)
-        
-        return render_template("0.html",showcase=(result),showcase1=(x))
-def nutrition(index):
-    
-    import requests
+def launch(img_filepath):
+    model = load_model('nutrition.h5')
+    img = load_img(img_filepath, target_size=(64, 64))
+    x = img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    predict_x = model.predict(x)
+    classes_x = np.argmax(predict_x)
+    index = ['Apple', 'Banana', 'Orange', 'Pineapple', 'Watermelon']
+    values = nutrition(index[classes_x])
+    return [index[classes_x], values]
 
-    url = "https://calorieninjas.p.rapidapi.com/v1/nutrition"
 
-    querystring = {"query":index}
+def nutrition(x):
+    conn = sqlite3.connect('nutri.db')
+    cursor = conn.execute(f'''SELECT * FROM NUTRI WHERE FRUIT=="{x}"''')
+    for row in cursor:
+       rec = {"FRUIT":row[0],"SERVING_SIZE":row[1],"ENERGY":row[2],"FAT":row[3],"SATURATED FAT":row[4], "MONO_UNSATURATED_FAT":row[5], "POLY_UNSATURATED_FAT":row[6],"CARBOHYDRATES":row[7],"SUGAR":row[8],"FIBER":row[9],"PROTEIN":row[10],"SODIUM":row[11],"CHOLESTEROL":row[12],"POTASSIUM":row[13],"OUTPUT":row[14]}
+    return rec
 
-    headers = {
-	"X-RapidAPI-Key": "85887549f4msh51e7315b280a87ep1f43e0jsn585c940f2ea6",
-	"X-RapidAPI-Host": "calorieninjas.p.rapidapi.com"
-     }
 
-    response = requests.request("GET", url, headers=headers, params=querystring)
-
-    print(response.text)
-    return response.json()['items']
 if __name__ == "__main__":
-    # running the app
     app.run(debug=False)
